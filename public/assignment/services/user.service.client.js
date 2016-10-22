c183f3616eee817fa6f08cd87221d764c8047fce
maloomis <loomis.m@husky.neu.edu>
2016-10-22 14:44:56 -0400

    started server side development

--- public/assignment/services/user.service.client.js
+++ public/assignment/services/user.service.client.js
@@ -3,14 +3,13 @@
         .module("WebAppMaker")
         .factory("UserService", UserService);
     
-    function UserService() {
-        
-        var users = [
-            {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
-            {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
-            {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
-            {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
-                    ];
+    function UserService($http) {
+        var users = [];
+
+        $http.get("/rest/course")
+        .success(function(response){
+            users = response;
+        });
         
         var api = {
             "createUser" : createUser,
--- server.js
+++ server.js
@@ -15,3 +15,17 @@ var port      = process.env.AWS_NODEJS_PORT || 3000;
 
 console.log("Port Number", port);
 app.listen(port, ipaddress);
+
+require("/assignment/app.js")(app);
+app.listen(port, ipaddress);
+
+app.get("/rest/user", function (req, res) {
+    var users = [
+    {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
+    {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
+    {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
+    {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
+    ];
+
+    res.send(users);
+});
